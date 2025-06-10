f02ca4007a6f5fcad07c4ff7877db48addc7a60b
Nicolas Housset <nicolas.housset@gmail.com>
2025-06-10 10:03:51 +0200

    add list nodes module

---
+++ nodes_list.bat
@@ -0,0 +1,16 @@
+@echo off
+TITLE Liste des Pods OpenShift
+
+ECHO Lancement du script de listing des pods...
+ECHO.
+
+REM %~dp0 est un chemin magique qui pointe vers le dossier du .bat lui-même (ici, src\)
+REM MODIFICATION : On pointe vers le nouveau script list_pods.ps1
+SET "SCRIPT_PATH=%~dp0\src\list_nodes.ps1"
+
+REM Exécute le script PowerShell
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%"
+
+ECHO.
+ECHO Le script PowerShell est terminé.
+pause
