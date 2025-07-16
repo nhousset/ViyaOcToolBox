85c1ebd9c9b426309ad55fa06a66c44ac8ffefe8
Nicolas Housset <nicolas.housset@gmail.com>
2025-07-16 09:36:05 +0200

    Update list_pods.ps1

--- src/list_pods.ps1
+++ src/list_pods.ps1
@@ -2,10 +2,9 @@
 . "$PSScriptRoot\functions.ps1"
 
 $params = Initialize-And-Validate-Connection
-Set-OpenShiftProject -OcPath $params.OcPath -Namespace $params.DefaultNamespace
+
 
 Write-Host "`n--- Liste des pods en cours d'exécution (Running) ---" -ForegroundColor Cyan
 $getPodsArguments = @("get", "pods", "--field-selector=status.phase=Running")
 
-Execute-OcCommand -OcPath $getPodsArguments
-
+Execute-OcCommand $getPodsArguments
