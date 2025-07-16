5df00a25f85b074f7df43ffdc6fa9407a5d66725
Nicolas Housset <nicolas.housset@gmail.com>
2025-07-16 09:38:16 +0200

    Create viya_check_cas_services.bat

---
+++ viya_check_cas_services.bat
@@ -0,0 +1,13 @@
+@echo off
+TITLE Compute Pod Interaction
+
+ECHO Lancement du script interactif Compute...
+ECHO.
+
+SET "SCRIPT_PATH=%~dp0\src\viya_check_cas.ps1"
+
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%"
+
+ECHO.
+ECHO Le script PowerShell est termine.
+pause
